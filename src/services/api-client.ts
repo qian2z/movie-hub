5920ec0159881d4d0bae6d8bbe33ee5ce56b3c59
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 21:22:56 +0800

    Update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: import.meta.env.TMDB_API_KEY,
+    api_key: "a4ce3cbe6fa0a42b246040bfbcef6b4a",
   },
 });
