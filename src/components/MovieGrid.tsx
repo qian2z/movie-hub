841291ef2d559f735329b025185af38f0c4990f2
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 14:10:03 +0800

    Create Custom Hook to Fetch Movies

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Movie {
-  id: number;
-  title: string;
-}
-
-interface FetchMovieResponse {
-  page: number;
-  results: Movie[];
-}
+import useMovies from "../hooks/useMovies";
 
 const MovieGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchMovieResponse>("/discover/movie/")
-      .then((res) => setMovies(res.data.results))
-      .catch((err) => setError(err));
-  }, []);
+  const { movies, error } = useMovies();
 
   return (
     <>
