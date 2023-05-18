62f741c07526ebaca925cc418179f8f31bea5cf4
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 18:47:13 +0800

    Fixed Language Sorting

--- src/hooks/useLanguges.ts
+++ src/hooks/useLanguges.ts
@@ -6,6 +6,13 @@ export interface Language {
 }
 
 // const useLanguages = () => useData<Language>("/configuration/languages");
-const useLanguages = () => ({ data: languages, error: null, isLoading: false });
+const useLanguages = () => ({
+  data: languages.sort(
+    (a: { english_name: string }, b: { english_name: string }) =>
+      a.english_name.localeCompare(b.english_name)
+  ),
+  error: null,
+  isLoading: false,
+});
 
 export default useLanguages;
