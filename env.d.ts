6c7cafb68ea4af94aea0600c35465fab76676d1b
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 21:35:16 +0800

    Update Image

--- env.d.ts
+++
@@ -1,3 +0,0 @@
-interface ImportMetaEnv {
-  TMDB_API_KEY: string;
-}
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,6 @@
-import noImage from "../assets/no-image-placeholder.webp";
-
 const getImageUrl = (url: string) => {
-  if (!url) return noImage;
+  if (!url)
+    return "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
   const baseURL = "https://image.tmdb.org/t/p/w500";
   return baseURL + url;
 };
