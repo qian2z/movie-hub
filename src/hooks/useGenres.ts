393372f2504da51d36c6c46d76f6ce00fb358cf7
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 18:36:12 +0800

    Ship Genres and Languages as Static

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,11 @@
-import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
   name: string;
 }
 
-const useGenres = () => useData<Genre>("/genre/movie/list");
+// const useGenres = () => useData<Genre>("/genre/movie/list");
+const useGenres = () => ({ data: genres, error: null, isLoading: false });
 
 export default useGenres;
--- src/hooks/useLanguges.ts
+++ src/hooks/useLanguges.ts
@@ -1,10 +1,11 @@
-import useData from "./useData";
+import languages from "../data/languages";
 
 export interface Language {
   iso_639_1: string;
   english_name: string;
 }
 
-const useLanguages = () => useData<Language>("/configuration/languages");
+// const useLanguages = () => useData<Language>("/configuration/languages");
+const useLanguages = () => ({ data: languages, error: null, isLoading: false });
 
 export default useLanguages;
