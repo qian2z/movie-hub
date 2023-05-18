01ea4986213d47cb99950307b825488076235f81
qian2z <98630339+qian2z@users.noreply.github.com>
2023-05-18 18:37:21 +0800

    Refactor: Movie Grid

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -13,28 +13,27 @@ const MovieGrid = ({ movieQuery }: Props) => {
   const { data, error, isLoading } = useMovies(movieQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3 }}
-        padding="10px"
-        spacing={8}
-        marginRight={8}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <MovieCardContainer key={skeleton}>
-              <MovieCardSkeleton />
-            </MovieCardContainer>
-          ))}
-        {data.map((movie) => (
-          <MovieCardContainer key={movie.id}>
-            <MovieCard movie={movie}></MovieCard>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3 }}
+      padding="10px"
+      spacing={8}
+      marginRight={8}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <MovieCardContainer key={skeleton}>
+            <MovieCardSkeleton />
           </MovieCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((movie) => (
+        <MovieCardContainer key={movie.id}>
+          <MovieCard movie={movie}></MovieCard>
+        </MovieCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
