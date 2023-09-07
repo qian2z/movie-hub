1ee6947929d3c47c828dfea3c373231660021c0e
qian2z <98630339+qian2z@users.noreply.github.com>
2023-09-07 21:22:17 +0800

    Handle Error

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Layout from "./pages/Layout";
 import MovieDetailsPage from "./pages/MovieDetailsPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
       { path: "movies/:id", element: <MovieDetailsPage /> },
