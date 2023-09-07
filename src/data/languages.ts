857d70c707f8f7be62ae1522b75f8f7fccbde26e
qian2z <98630339+qian2z@users.noreply.github.com>
2023-09-07 15:09:48 +0800

    Refactor Languages Hook

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -9,19 +9,4 @@ export default [
     english_name: "Mandarin",
     name: "普通话",
   },
-  {
-    iso_639_1: "th",
-    english_name: "Thai",
-    name: "ภาษาไทย",
-  },
-  {
-    iso_639_1: "ja",
-    english_name: "Japanese",
-    name: "日本語",
-  },
-  {
-    iso_639_1: "ko",
-    english_name: "Korean",
-    name: "한국어/조선말",
-  },
 ];
--- src/hooks/useLanguges.ts
+++ src/hooks/useLanguges.ts
@@ -5,12 +5,8 @@ export interface Language {
   english_name: string;
 }
 
-// const useLanguages = () => useData<Language>("/configuration/languages");
 const useLanguages = () => ({
-  data: languages.sort(
-    (a: { english_name: string }, b: { english_name: string }) =>
-      a.english_name.localeCompare(b.english_name)
-  ),
+  data: languages,
   error: null,
   isLoading: false,
 });
